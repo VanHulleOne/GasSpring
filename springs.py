9b6d2e534657e21fad5c29aefd30a8b0d60f7993
lvanhulle <lvanhulle@wisc.edu>
2019-04-10 13:33:06 -0500

    Added filters and removed old code

--- springs.py
+++ springs.py
@@ -162,7 +162,33 @@ def minMaxFilter(inSprings, minMaxDict):
         if values[0] == 0 and values[1] == INF:
             continue
         inSprings = [spring for spring in inSprings if values[0] <= getattr(spring, key).magnitude <= values[1]]
+    print('After minmaxFilter:', len(inSprings))
     return inSprings
+
+def optionsFilter(inSprings, options):
+    for key, value in options.items():
+        if value is None:
+            continue
+        inSprings = [spring for spring in inSprings if getattr(spring, key) == value]
+    print('After optionsFilter:', len(inSprings))
+    return inSprings
+
+def lifeFilter(inSprings, length1, length2, minLife):
+    values = sorted(l for l in [length1, length2] if l is not None)
+    if values:
+        inSprings = [s for s in inSprings if s.expectedLife(values[0]*ureg.inch) >= minLife]
+    print('After lifeFilter:', len(inSprings))
+    return inSprings
+        
+def forceFilter(inSprings, lengths, forces, tolerances):
+    for length, force, tol in zip(lengths, forces, tolerances):
+        if length is None:
+            continue
+        inSprings = [s for s in inSprings if (1-tol)*force <= s.getForce(length) <= (1+tol)*force]        
+    print('After forceFilter:', len(inSprings))
+    return inSprings
+    
+
         
             
 def getSprings2(thisSprings,*,
@@ -176,128 +202,61 @@ def getSprings2(thisSprings,*,
                 length2=None,
                 force1=None,
                 force2=None,
-                safeSolidLength=False, # If the max deflection is at the solid length the spring can't be wrecked by over compression
+                safeSolidLength=None, # If the max deflection is at the solid length the spring can't be wrecked by over compression
                 material=None,
                 ends='CG',
                 finish=None,
-                tolerance=0.2,
-                numResults=INF
+                tolerance1=0.2,
+                tolerance2=0.2,
+                numResults=INF,
+                minLife=L_INF,
                 ):
     minMaxDict = {'OD':[minOD, maxOD],
                   'ID':[minID, maxID],
-                  'freeLength':[minFreeLength, maxFreeLength]}
+                  'freeLength':[minFreeLength, maxFreeLength],
+                  }
+    optionsDict = {'material':material,
+                   'ends':ends,
+                   'finish': finish,
+                   'safeSolidLength':safeSolidLength,
+                   }
+    
+    thisSprings = optionsFilter(thisSprings, optionsDict)
     thisSprings = minMaxFilter(thisSprings, minMaxDict)
+    thisSprings = lifeFilter(thisSprings, length1, length2, minLife)
+    thisSprings = forceFilter(thisSprings, [length1, length2], [force1, force2], [tolerance1, tolerance2])
+
     return thisSprings
-    
 
-def filterer(spring, *, units=IMP,
-             OD=None,
-             length=None,
-             ID=None,
-             rate=None,
-             deflection=None,
-             load=None,
-             solidLength=None,
-             wireDia=None,
-             totalCoils=None,
-             material=None,
-             ends=None,
-             finish=None):
-    
-    for key, value in locals().items():
-        if value is not None:
-            if key in options:
-                if getattr(spring, key) != value:
-                    return False
-            elif key in offsets:
-                springValue = spring[offsets[key]+units]
-                if springValue < value[MIN] or springValue > value[MAX]:#value[MIN] > springValue > value[MAX]:
-                    return False
-            elif key == 'totalCoils':
-                raise Exception('Total Coils not implimented')
-#                if value[MIN] > spring.totalCoils > value[MAX]:
-#                    return False
-    return True
-
-#print(filterer(springs[0], OD=(0, 0.04)))
-
-def getSprings(*,
-               units=IMP,
-               OD=None,
-               ID=None,
-               length1=None,
-               length2=None,
-               force1=None,
-               force2=None,
-               tol=0.2,
-               material=None,
-               ends=None):
-    rate = (force2-force1)/(length1-length2)
-    filteredSprings = [spring for spring in springs if filterer(spring,
-                                                                units=units,
-                                                                OD=OD,
-                                                                ID=ID,
-                                                                solidLength=[0, length2],
-                                                                length=[length1, INF],
-                                                                rate=[(1-tol)*rate, (1+tol)*rate],
-                                                                material=material,
-                                                                ends=ends
-                                                                )]
-    print('Num filtered:', len(filteredSprings))
-    doubleFilter = []
-    for spring in filteredSprings:
-        rateAct = spring[offsets['rate']+units]
-        freeLength = spring[offsets['length']+units]
-        f1Act = (freeLength-length1)*rateAct
-        if (1-tol)*force1 < f1Act < (1+tol)*force1:
-            doubleFilter.append(spring)
-            
-    print('Num double:', len(doubleFilter))
-    fatigueFiltered = [s for s in doubleFilter if fatigueLimit(s, length1, length2)[0]]
-            
-    return fatigueFiltered
-
-def fatigueLimit(spring, l1, l2, units=IMP):
-    _id = spring[offsets['ID']+units]
-    wireDia = spring[offsets['wireDia']+units]
-    tensileStrength = matProperties[spring.material][0]
-    freeLength = spring[offsets['length']+units]
-    rate = spring[offsets['rate']+units]
-
-    K = (4.0*_id/wireDia - 1)/(4.0*_id/wireDia - 4) + 0.615*wireDia/_id
-    
-    f1 = (freeLength - l1)*rate
-    f2 = (freeLength - l2)*rate
-    
-    sig1 = 8*f1*_id*K/(pi*wireDia**3)
-    sig2 = 8*f2*_id*K/(pi*wireDia**3)
-    
-    R1 = sig1/tensileStrength
-    R2 = sig2/tensileStrength
-    
-    if R1 > 0.34 and R2 < 0.48:
-        return True, R1, R2
-    if R2 < (0.4*R1 + 0.34):
-        return True, R1, R2
-    return False, R1, R2
-
-#fs = getSprings(ID=[0.4,0.6], length1=2, length2=1, force1=2, force2=10, material='SST', ends='CG')
-    
-#stripSprings = getSprings(ID=[0.323, INF], OD=[0, 0.49], length1=2.046, length2=1.862,
-#                          force1=4, force2=8, material='SST', ends='CG', tol=0.3)
-#print('NumSprings', len(stripSprings))
-
-#retSprings = getSprings(ID=[0.403, INF], OD=[0, 0.58], length1=0.531, length2=0.374,
-#                       force1=0.7, force2=1.0, material='SST', ends='CG', tol=0.25)
-#print('NumSprings', len(retSprings))
-
-
-
-s = getSprings2(springs, minOD=.1, maxOD=.11)
+s = getSprings2(springs, minOD=.1, maxOD=.11, length1=0.25)
 
 print('Length:', len(s))
 
 
 
 
+
+#def fatigueLimit(spring, l1, l2, units=IMP):
+#    _id = spring[offsets['ID']+units]
+#    wireDia = spring[offsets['wireDia']+units]
+#    tensileStrength = matProperties[spring.material][0]
+#    freeLength = spring[offsets['length']+units]
+#    rate = spring[offsets['rate']+units]
+#
+#    K = (4.0*_id/wireDia - 1)/(4.0*_id/wireDia - 4) + 0.615*wireDia/_id
+#    
+#    f1 = (freeLength - l1)*rate
+#    f2 = (freeLength - l2)*rate
+#    
+#    sig1 = 8*f1*_id*K/(pi*wireDia**3)
+#    sig2 = 8*f2*_id*K/(pi*wireDia**3)
+#    
+#    R1 = sig1/tensileStrength
+#    R2 = sig2/tensileStrength
+#    
+#    if R1 > 0.34 and R2 < 0.48:
+#        return True, R1, R2
+#    if R2 < (0.4*R1 + 0.34):
+#        return True, R1, R2
+#    return False, R1, R2
 
